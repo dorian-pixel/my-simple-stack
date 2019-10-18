3a48deee91518a430a99756ec89d501477809445
dorian-pixel <dorian.azema@gmail.com>
2019-10-18 09:43:24 +0200

    mySimpleStack1

---
+++ src/main/java/deqo/daze/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.daze.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
